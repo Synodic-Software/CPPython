49ae392d71be044efd835c9808bbd23164f6bf62
Asher Norland <asher.norland@gmail.com>
2021-04-03 19:56:24 -0400

    Yield the workspace

--- tests/fixtures/temporary.py
+++ tests/fixtures/temporary.py
@@ -4,25 +4,40 @@ from os import scandir
 from pathlib import Path
 from distutils.dir_util import copy_tree
 
+from typing import Generator
+
+
 def _extract_directories(directory):
-    directories = [Path(f) for f in scandir(directory) if f.is_dir() ]
+    directories = [Path(f) for f in scandir(directory) if f.is_dir()]
     return directories
+
 
 _directories = _extract_directories(Path("tests/data/project_templates").absolute())
 
+
 def pytest_generate_tests(metafunc):
 
-    # Provides a directory taken from the 'tests/data' directory
     if "directory" in metafunc.fixturenames:
-        metafunc.parametrize("directory", _directories, ids = [id.name for id in _directories])
+        metafunc.parametrize("directory", _directories, ids=[id.name for id in _directories])
 
 
 @pytest.fixture
-def tmp_workspace(tmp_path, directory):
-    '''
-    Load the dummy project to its initial state
-    '''
-    base_directory = Path(tmp_path).absolute()
-    copy_tree(str(directory), str(base_directory))
+def tmp_library(tmp_path: Path, directory: Path) -> Path:
+    """
+    @returns - A path to the temporary directory populated with a test library
+    """
+    target_directory = Path(tmp_path).absolute()
+    copy_tree(str(directory), str(target_directory))
 
-    return base_directory
+    return target_directory
+
+
+@pytest.fixture
+def tmp_workspace(tmp_library: Path) -> Generator[str, None, None]:
+    """
+    While the fixture is used, the current directory exists as a temporary workspace populated with a library and a virtual environment
+
+    @returns - The name of the library
+    """
+    with tmp_library:
+        yield tmp_library.name
--- tests/integration/test_project.py
+++ tests/integration/test_project.py
@@ -1,6 +1,8 @@
 from poetic_conan.plugin import SynodicPlugin
 from conans.client.conan_api import ConanAPIV1
 
+from poetry.poetry import Poetry
+
 class TestProjectValidation:
 
     def test_correct_project(self, tmp_workspace):
