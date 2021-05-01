b70b314d134446273283bb2aedbcd3150f4080f8
Asher Norland <asher.norland@gmail.com>
2021-05-01 02:15:18 -0400

    Partial Property Validation

--- cppoetry/utility.py
+++ cppoetry/utility.py
@@ -1,47 +1,74 @@
 from tomlkit.toml_document import TOMLDocument
 from tomlkit.exceptions import NonExistentKey
 
+from cerberus import Validator
+
 from pathlib import Path
 
 
-class Metadata:
-    def __init__(self, root: Path, document: TOMLDocument) -> None:
+class _BaseGenerator:
+    def __init__(self, metadata: "MetaData") -> None:
+        self._metadata = metadata
 
-        self.root = root.absolute()
-        self.document = document
-        self.dirty = False
 
-    def generate_conanfile(self) -> Path:
+class ConanGenerator(_BaseGenerator):
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
 
+    def write_file(self, path: Path) -> None:
         #     Generate a conanfile.py with the given path.
         #     The resulting recipe is TODO
-        path = self.root 
         path.mkdir(parents=True, exist_ok=True)
         with open(path / "conanfile.py", "w+") as file:
 
             # Process the Conan data into a Conan format
-            name = self.name
+            name = self._metadata.name
             name = name.replace("-", "")
 
-            dependencies = ["/".join(tup) for tup in self.dependencies.items()]
+            dependencies = ["/".join(tup) for tup in self._metadata.dependencies.items()]
             dependencies = ",".join(f'"{dep}"' for dep in dependencies)
 
             # Write the Conan data to file
             # TODO: Require the conan version that this plugin depends on
             contents = (
-                f'from conans import ConanFile, CMake\n'
-                f'\n'
-                f'required_conan_version = ">=1.36.0"'
-                f'\n'
-                f'class {name}Conan(ConanFile):\n'
+                f"from conans import ConanFile, CMake\n"
+                f"\n"
+                f'required_conan_version = ">=1.36.0"\n'
+                f"\n"
+                f"class {name}Conan(ConanFile):\n"
                 f'    settings = "os", "compiler", "build_type", "arch"\n'
-                f'    requires = {dependencies}\n'
+                f"    requires = {dependencies}\n"
                 f'    generators = ["cmake_find_package", "cmake_paths"]\n'
             )
 
             print(contents, file=file)
 
-        return Path()
+
+class Metadata:
+    def __init__(self, root: Path, document: TOMLDocument) -> None:
+
+        self._root = root.absolute()
+        self._document = document
+        self._validator = Validator()
+        self._generator = ConanGenerator()
+
+        self.dirty = False
+
+    def __getattr__(self, name):
+        try:
+            return self.__dict[name]
+        except KeyError:
+            msg = "'{0}' object has no attribute '{1}'"
+            raise AttributeError(msg.format(type(self).__name__, name))
+
+    def __setattr__(self, name, value):
+        if name in self.data:
+            self.data[name] = value
+        else:
+            super().__setattr__(name, value)
+
+    def generate_conanfile(self) -> None:
+        self._generator.write_file(self._root)
 
     @property
     def name(self) -> str:
@@ -103,7 +130,7 @@ class Metadata:
             self._install_directory = Path("build")
 
         if not self._install_directory.is_absolute():
-            self._install_directory = self.root / self._install_directory 
+            self._install_directory = self.root / self._install_directory
 
         return self._install_directory
 
