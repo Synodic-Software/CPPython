c60459c95691a2e40e4aab245f5f8222ed7beb83
Asher Norland <asher.norland@gmail.com>
2021-05-05 20:17:07 -0400

    te

--- cppoetry/utility.py
+++ cppoetry/utility.py
@@ -2,6 +2,7 @@ from tomlkit.toml_document import TOMLDocument
 from tomlkit.exceptions import NonExistentKey
 
 from pathlib import Path
+from typing import Callable
 
 
 class _BaseGenerator:
@@ -43,51 +44,61 @@ class ConanGenerator(_BaseGenerator):
 
 
 class Metadata:
+
+    # Schema for poetry values
+    _poetry_schema = {
+        "name": {"type": "string"},
+        "version": {"type": "string"},  # TODO: Make Version type
+    }
+
+    # Schema for Conan values
+    _conan_schema = {
+        "remotes": {"type": "list", "schema": {"type": "tuple", "items": ({"type": "string"}, {"type": "string"})}},
+        "dependencies": {
+            "type": "list",
+            "schema": {
+                "type": "tuple",
+                "items": ({"type": "string"}, {"type": "string"}),  # TODO: Make Version type
+            },
+        },
+        "install_directory": {"type": "string"},  # TODO: Make Path type
+    }
+
+    _frozen_variables = {
+        "_root",
+        "_validator",
+        "_generator",
+        "_poetry_data",
+        "_conan_data",
+        "dirty",
+    }
+
     def __init__(self, root: Path, document: TOMLDocument) -> None:
 
         from cerberus import Validator
 
-        self._root = root.absolute()
+        self._root = root.absolute()  # TODO: Only required for Generator
         self._validator = Validator()
-        self._generator = ConanGenerator()
+        self._generator = ConanGenerator(document)  # TODO: Make external to Metadata
 
         # Gather data from the document
         # TODO: Error handling for these document tabs
         self._poetry_data = document["tool"]["poetry"]
         self._conan_data = document["tool"]["conan"]
 
-        # Schema for poetry values
-        self._poetry_schema = {
-            "name": {"type": "string"},
-            "version": {"type": "string"},  # TODO: Make Version type
-        }
-
-        # Schema for Conan values
-        self._conan_schema = {
-            "remotes": {"type": "list", "schema": {"type": "tuple", "items": ({"type": "string"}, {"type": "string"})}},
-            "dependencies": {
-                "type": "list",
-                "schema": {
-                    "type": "tuple",
-                    "items": ({"type": "string"}, {"type": "string"}),  # TODO: Make Version type
-                },
-            },
-            "install_directory": {"type": "string"},  # TODO: Make Path type
-        }
-
         self.dirty = False
 
     def __getattr__(self, name):
 
-        if name in self._poetry_schema:
-            if not self._validator.validate(self._poetry_data, self._poetry_schema[name]):
+        if name in Metadata._poetry_schema:
+            if not self._validator.validate(self._poetry_data, Metadata._poetry_schema[name]):
                 return self._poetry_data[name]
 
             msg = f"'{type(self).__name__}' failed Poetry validation with attribute '{name}'"
             raise AttributeError(msg)
 
-        if name in self._conan_schema:
-            if not self._validator.validate(self._conan_data, self._conan_schema[name]):
+        if name in Metadata._conan_schema:
+            if not self._validator.validate(self._conan_data, Metadata._conan_schema[name]):
                 return self._conan_data[name]
 
             msg = f"'{type(self).__name__}' failed Conan validation with attribute '{name}'"
@@ -97,14 +108,19 @@ class Metadata:
         raise AttributeError(msg)
 
     def __setattr__(self, name, value):
-        if name in self._poetry_data:
-            self.dirty = True
-            self._poetry_data[name] = value
-        elif name in self._conan_data:
-            self.dirty = True
-            self._conan_data[name] = value
-        else:
+        if name in self._frozen_variables:
             super().__setattr__(name, value)
+
+        else:
+            if name in self._poetry_data:
+                self.dirty = True
+                self._poetry_data[name] = value
+            elif name in self._conan_data:
+                self.dirty = True
+                self._conan_data[name] = value
+            else:
+                msg = f"'{type(self).__name__}' object has no attribute '{name}'"
+                raise AttributeError(msg)
 
     def generate_conanfile(self) -> None:
         self._generator.write_file(self._root)
