69c08503605ba6863e98d175974bf3e7d5064a8f
Asher Norland <asher.norland@gmail.com>
2022-01-14 14:23:28 -0500

    Ya

--- cppython/plugins/test/pytest.py
+++ cppython/plugins/test/pytest.py
@@ -1,21 +1,12 @@
 """
 TODO:
 """
-from abc import ABC
+from abc import ABC, abstractmethod
 from typing import Type
 
 import pytest
 
 from cppython.schema import Generator, Interface
-
-
-@pytest.fixture
-def generator() -> Type[Generator]:
-    """
-    A hook allowing implementations to override the fixture with a parameterization
-        @pytest.mark.parametrize("generator", [CustomGenerator])
-    """
-    raise NotImplementedError
 
 
 class BaseGeneratorSuite(ABC):
@@ -24,11 +15,23 @@ class BaseGeneratorSuite(ABC):
     This class provides a generic test suite that all custom types must function with.
     """
 
-    def test_construction(self, generator):
+    @abstractmethod
+    def __init__(self, generator_type: Type[Generator]) -> None:
+        super().__init__()
+
+    @pytest.fixture
+    def generator(self) -> Type[Generator]:
+        """
+        A hook allowing implementations to override the fixture with a parameterization
+            @pytest.mark.parametrize("generator", [CustomGenerator])
+        """
+        raise NotImplementedError
+
+    def test_construction(self):
         """
         Test the __init__ call of the generator object
         """
-        generator()
+        self.generator()
 
 
 @pytest.fixture
@@ -45,3 +48,9 @@ class BaseInterfaceSuite(ABC):
     Custom implementations of the Interface class should inherit from this class for its tests.
     This class provides a generic test suite that all custom types must function with.
     """
+
+    def test_construction(self, interface):
+        """
+        Test the __init__ call of the interface object
+        """
+        interface()
--- tests/unit/test_generator.py
+++ tests/unit/test_generator.py
@@ -8,11 +8,13 @@ from cppython.plugins.generator.cmake import CMakeGenerator
 from cppython.plugins.test.pytest import BaseGeneratorSuite
 
 
-@pytest.mark.parametrize("generator", [CMakeGenerator])
 class TestCMakeGenerator(BaseGeneratorSuite):
     """
     The tests for our CMake generator
     """
+
+    def __init__(self) -> None:
+        super().__init__(CMakeGenerator)
 
     def test_name(self, generator):
         """
