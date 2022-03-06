49f99f562956602603755e72d5c083cdcc7eacc7
Asher Norland <asher.norland@gmail.com>
2022-03-06 17:17:36 -0500

    Update data.py

--- cppython/plugins/test/data.py
+++ cppython/plugins/test/data.py
@@ -9,7 +9,6 @@ from cppython.schema import PEP621, CPPythonData, PyProject, TargetEnum
 default_pep621 = PEP621(name="test_name", version="1.0")
 
 # CMake is a default plugin
-# TODO: Provide dynamic default
 default_cppython_data = CPPythonData(**{"generator": "cmake", "target": TargetEnum.EXE, "install-path": Path()})
 
 default_pyproject = PyProject(project=default_pep621, cppython=default_cppython_data)
