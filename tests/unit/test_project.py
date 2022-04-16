4876fca88bd2ff378fcf6316514143984c1f5caf
Asher Norland <asher.norland@gmail.com>
2022-04-16 13:28:19 -0400

    Update Path Tests (#41)

--- tests/unit/test_project.py
+++ tests/unit/test_project.py
@@ -31,10 +31,29 @@ class TestProject:
         configuration = ProjectConfiguration()
         Project(configuration, interface_mock, default_pyproject.dict(by_alias=True))
 
-    def test_download(self):
+    def test_download(self, mocker: MockerFixture):
         """
         TODO
         """
+
+        interface_mock = mocker.MagicMock()
+        configuration = ProjectConfiguration()
+
+        generator_type = mocker.Mock(spec=Generator)
+        generator_type.name.return_value = "mock"
+        generator_type.data_type.return_value = MockGeneratorData
+
+        gather_override = mocker.patch.object(ProjectBuilder, "gather_plugins")
+        gather_override.return_value = [generator_type]
+
+        project_data = default_pyproject.dict(by_alias=True)
+        mock_data = MockGeneratorData(check=True)
+        project_data["tool"]["cppython"]["mock"] = mock_data.dict(by_alias=True)
+
+        project = Project(configuration, interface_mock, project_data)
+
+        # TODO: This does not verify signature correctness
+        project.download()
 
 
 class TestBuilder:
