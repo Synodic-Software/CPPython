51a59650a5fefca458706d2b149eb259c91b5353
Asher Norland <asher.norland@gmail.com>
2022-05-08 13:18:37 -0400

    JSON: Return Filename, Not Directory

--- cppython/utility.py
+++ cppython/utility.py
@@ -8,18 +8,22 @@ from pathlib import Path
 from cppython.schema import CMakePresets
 
 
-def read_preset(path: Path) -> CMakePresets:
+def read_preset(name: str, path: Path) -> CMakePresets:
     """
     Reading routing
     """
 
-    preset_path = path / "CMakePresets.json"
+    preset_path = path / f"{name}.json"
     return CMakePresets.parse_file(path=preset_path)
 
 
-def write_preset(path: Path, presets: CMakePresets) -> None:
+def write_preset(name: str, path: Path, presets: CMakePresets) -> Path:
     """
     Writing routine
     """
-    with open(path / "CMakePresets.json", "w", encoding="utf8") as json_file:
+    file = path / f"{name}.json"
+
+    with open(file, "w", encoding="utf8") as json_file:
         json.dump(presets.dict(), json_file, ensure_ascii=False, indent=2)
+
+    return file
