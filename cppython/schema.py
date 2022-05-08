a98390ee290e32ff0fb05dfe5785fde03afb7f1b
Asher Norland <asher.norland@gmail.com>
2022-05-08 10:21:49 -0400

    Add CMake Schema

--- cppython/schema.py
+++ cppython/schema.py
@@ -6,6 +6,72 @@ from __future__ import annotations  # Required for self-referenced pydantic type
 
 from abc import abstractmethod
 from dataclasses import dataclass
+from pathlib import Path
+from typing import Any, Optional
+
+from pydantic import BaseModel, Extra, Field
+
+
+class Preset(BaseModel):
+    """
+    Partial Preset specification
+    """
+
+    name: str
+    hidden: Optional[bool]
+    inherits: Optional[list[Preset]]
+    displayName: Optional[str]
+    description: Optional[str]
+
+
+class ConfigurePreset(Preset):
+    """
+    Partial Configure Preset specification
+    """
+
+    toolchainFile: Optional[Path]
+
+
+class BuildPreset(Preset):
+    """
+    Partial Build Preset specification
+    """
+
+    configurePreset: Optional[str]
+    inheritConfigureEnvironment: Optional[bool] = True
+
+
+class TestPreset(Preset):
+    """
+    Partial Test Preset specification
+    """
+
+    configurePreset: Optional[str]
+    inheritConfigureEnvironment: Optional[bool] = True
+
+
+class CMakeVersion(BaseModel, extra=Extra.forbid):
+    """
+    The version specification for CMake
+    """
+
+    major: int = 3
+    minor: int = 23
+    patch: int = 1
+
+
+class CMakePresets(BaseModel, extra=Extra.forbid):
+    """
+    The schema for the CMakePresets and CMakeUserPresets files
+    """
+
+    version: int = Field(4, const=True)
+    cmakeMinimumRequired: Optional[CMakeVersion]
+    include: Optional[list[str]]
+    vendor: Optional[Any]
+    configurePresets: Optional[list[ConfigurePreset]]
+    buildPresets: Optional[list[BuildPreset]]
+    testPresets: Optional[list[TestPreset]]
 
 
 @dataclass
