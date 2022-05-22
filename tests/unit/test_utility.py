2c18938ce6309e23b16739a47de7a4c2e061cad0
Asher Norland <asher.norland@gmail.com>
2022-05-22 04:32:13 -0400

    Update Root CMakePreset Writes

--- tests/unit/test_utility.py
+++ tests/unit/test_utility.py
@@ -13,26 +13,26 @@ class TestBuilder:
     TODO
     """
 
+    class TestModel(BaseModel):
+        """
+        TODO
+        """
+
+        test_path: Path
+        test_int: int
+
     def test_model_read_write(self, tmpdir):
         """
         TODO
         """
 
-        class TestModel(BaseModel):
-            """
-            TODO
-            """
-
-            test_path: Path
-            test_int: int
-
-        test_model = TestModel(test_path=Path(), test_int=3)
+        test_model = TestBuilder.TestModel(test_path=Path(), test_int=3)
 
         temporary_directory = Path(tmpdir)
 
         json_path = temporary_directory / "test.json"
 
         write_model_json(json_path, test_model)
-        output = read_model_json(json_path, TestModel)
+        output = read_model_json(json_path, TestBuilder.TestModel)
 
         assert test_model == output
