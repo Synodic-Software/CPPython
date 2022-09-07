29b64a8507ac6519581e717cd55b8001a2aaf7ae
Asher Norland <asher.norland@gmail.com>
2022-09-07 08:45:35 -0400

    Update Chore

--- cppython/project.py
+++ cppython/project.py
@@ -13,7 +13,6 @@ from cppython_core.schema import (
     PEP621Resolved,
     ProjectConfiguration,
 )
-from cppython_core.utility import cppython_logger
 
 from cppython.builder import Builder
 from cppython.schema import API
@@ -34,34 +33,35 @@ class Project(API):
 
         # Add default output stream
         console_handler = logging.StreamHandler()
-        cppython_logger.addHandler(console_handler)
-        cppython_logger.setLevel(levels[configuration.verbosity])
+        self.logger = logging.getLogger("cppython")
+        self.logger.addHandler(console_handler)
+        self.logger.setLevel(levels[configuration.verbosity])
 
-        cppython_logger.info("Initializing project")
+        self.logger.info("Initializing project")
 
         self._builder = Builder(self.configuration)
         plugins = self._builder.gather_plugins(Generator)
 
         if not plugins:
-            cppython_logger.error("No generator plugin was found")
+            self.logger.error("No generator plugin was found")
             return
 
         for plugin in plugins:
-            cppython_logger.warning(f"Generator plugin found: {plugin.name()}")
+            self.logger.warning(f"Generator plugin found: {plugin.name()}")
 
         extended_pyproject_type = self._builder.generate_model(plugins)
         pyproject = extended_pyproject_type(**pyproject_data)
 
         if pyproject is None:
-            cppython_logger.error("Data is not defined")
+            self.logger.error("Data is not defined")
             return
 
         if pyproject.tool is None:
-            cppython_logger.error("Table [tool] is not defined")
+            self.logger.error("Table [tool] is not defined")
             return
 
         if pyproject.tool.cppython is None:
-            cppython_logger.error("Table [tool.cppython] is not defined")
+            self.logger.error("Table [tool.cppython] is not defined")
             return
 
         self._enabled = True
@@ -79,7 +79,7 @@ class Project(API):
             plugins, self.configuration, generator_configuration, self.project, self.cppython
         )
 
-        cppython_logger.info("Initialized project successfully")
+        self.logger.info("Initialized project successfully")
 
     @property
     def enabled(self) -> bool:
@@ -114,7 +114,7 @@ class Project(API):
         Download the generator tooling if required
         """
         if not self._enabled:
-            cppython_logger.info("Skipping 'download_generator_tools' because the project is not enabled")
+            self.logger.info("Skipping 'download_generator_tools' because the project is not enabled")
             return
 
         base_path = self.cppython.install_path
@@ -125,20 +125,20 @@ class Project(API):
             path.mkdir(parents=True, exist_ok=True)
 
             if not generator.generator_downloaded(path):
-                cppython_logger.warning(f"Downloading the {generator.name()} requirements to {path}")
+                self.logger.warning(f"Downloading the {generator.name()} requirements to {path}")
 
                 # TODO: Make async with progress bar
                 generator.download_generator(path)
-                cppython_logger.warning("Download complete")
+                self.logger.warning("Download complete")
             else:
-                cppython_logger.info(f"The {generator.name()} generator is already downloaded")
+                self.logger.info(f"The {generator.name()} generator is already downloaded")
 
     def update_generator_tools(self) -> None:
         """
         Update the generator tooling if available
         """
         if not self._enabled:
-            cppython_logger.info("Skipping 'update_generator_tools' because the project is not enabled")
+            self.logger.info("Skipping 'update_generator_tools' because the project is not enabled")
             return
 
         self.download_generator_tools()
@@ -156,27 +156,27 @@ class Project(API):
         TODO
         """
         if not self._enabled:
-            cppython_logger.info("Skipping install because the project is not enabled")
+            self.logger.info("Skipping install because the project is not enabled")
             return
 
-        cppython_logger.info("Installing tools")
+        self.logger.info("Installing tools")
         self.download_generator_tools()
 
-        cppython_logger.info("Installing project")
+        self.logger.info("Installing project")
         preset_path = self.cppython.build_path
 
         generator_output = []
 
         # TODO: Async
         for generator in self._generators:
-            cppython_logger.info(f"Installing {generator.name()} generator")
+            self.logger.info(f"Installing {generator.name()} generator")
 
             try:
                 generator.install()
                 config_preset = generator.generate_cmake_config()
                 generator_output.append((generator.name(), config_preset))
             except Exception as exception:
-                cppython_logger.error(f"Generator {generator.name()} failed to install")
+                self.logger.error(f"Generator {generator.name()} failed to install")
                 raise exception
 
         project_presets = self._builder.write_presets(preset_path, generator_output)
@@ -187,13 +187,13 @@ class Project(API):
         TODO
         """
         if not self._enabled:
-            cppython_logger.info("Skipping update because the project is not enabled")
+            self.logger.info("Skipping update because the project is not enabled")
             return
 
-        cppython_logger.info("Updating tools")
+        self.logger.info("Updating tools")
         self.update_generator_tools()
 
-        cppython_logger.info("Updating project")
+        self.logger.info("Updating project")
 
         preset_path = self.cppython.build_path
 
@@ -201,14 +201,14 @@ class Project(API):
 
         # TODO: Async
         for generator in self._generators:
-            cppython_logger.info(f"Updating {generator.name()} generator")
+            self.logger.info(f"Updating {generator.name()} generator")
 
             try:
                 generator.update()
                 config_preset = generator.generate_cmake_config()
                 generator_output.append((generator.name(), config_preset))
             except Exception as exception:
-                cppython_logger.error(f"Generator {generator.name()} failed to update")
+                self.logger.error(f"Generator {generator.name()} failed to update")
                 raise exception
 
         project_presets = self._builder.write_presets(preset_path, generator_output)
