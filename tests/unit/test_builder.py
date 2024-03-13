0a09ce0fa06f33b1aed9fbb228e99664fe2421a8
Asher Norland <asher.norland@gmail.com>
2024-03-13 18:07:00 -0400

    Add Tests

---
+++ tests/unit/test_builder.py
@@ -0,0 +1,21 @@
+"""Tests the Builder and Resolver types"""
+
+import pytest
+
+from cppython.builder import Builder, Resolver
+
+
+class TestBuilder:
+    """Various tests for the Builder type"""
+
+    def test_build(self) -> None:
+        """Verifies that the builder can build a project"""
+        pytest.fail("Test not implemented")
+
+
+class TestResolver:
+    """Various tests for the Resolver type"""
+
+    def test_generate_plugins(self) -> None:
+        """Verifies that the resolver can generate plugins"""
+        pytest.fail("Test not implemented")
---
+++ tests/unit/test_data.py
@@ -0,0 +1,11 @@
+"""Tests the Data type"""
+
+import pytest
+
+
+class TestData:
+    """Various tests for the Data type"""
+
+    def test_sync(self) -> None:
+        """Verifies that the data can be synchronized"""
+        pytest.fail("Test not implemented")
